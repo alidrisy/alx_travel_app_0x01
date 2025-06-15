40610502ceb23d209b61159ea4a53f1a0c8be6d7
Abdulrahman Alidrisy <a.alidrisy6@gmail.com>
2025-06-15 09:41:59 +0300

    feat: initialize Django project structure with basic configurations and files

---
+++ listings/models.py
@@ -0,0 +1,8 @@
+import uuid
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+class User(AbstractUser):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    first_name = models.CharField(max_length=15, blank=True, null=True)
+    last_name = models.CharField(max_length=15, blank=True, null=True)
