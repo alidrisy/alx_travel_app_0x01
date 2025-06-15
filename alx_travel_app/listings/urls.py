fd74e74e29a9b943c561178d0f6b9f443e8d1f86
Abdulrahman Alidrisy <a.alidrisy6@gmail.com>
2025-06-15 09:43:39 +0300

    feat: add initial listings app with user model and basic configurations

---
+++ alx_travel_app/listings/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+app_name = 'listings'
+
+urlpatterns = [
+    # Add your URL patterns here
+    # Example:
+    # path('', views.index, name='index'),
+] 
